94dca95a533be636fb33cf1c8092b9f4afd4df0d
Lucas Felix <lucas@sogilis.com>
2016-12-20 18:29:12 +0100

    introduce new arguments system

--- src/mavkit.cpp
+++ src/mavkit.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <unistd.h>
 #include <thread>
+#include <argp.h>
 
 #define BUFFER_LENGTH 512
 #define SYS_ID 1
@@ -31,39 +32,164 @@ std::vector<MavMessengerInterface*> outputs;
 //----------------------------------------------------------------------------//
 int main(int argc, char* argv[])
 {
-    // master = new MavlinkUDP("192.168.1.42", 14550);
-    // master = new MavlinkUDP(14550);
-    master = new MavlinkSerial("/dev/ttyACM0", 57600);
-    // master = new MavlinkLogReader("log/log1");
+    bool master_assigned = false;
 
-    MavMessengerInterface* messenger;
+    int c;
+    while (true)
+    {
+        static struct option long_options[] =
+        {
+            /* These options set a flag. */
+            //   {"verbose", no_argument,       &verbose_flag, 1},
+            //   {"brief",   no_argument,       &verbose_flag, 0},
+            /* These options don’t set a flag.
+            We distinguish them by their indices. */
+            {"tty",        required_argument, 0, 'a'},
+            {"udp_server", required_argument, 0, 'b'},
+            {"udp_client", required_argument, 0, 'c'},
+            {"file",       required_argument, 0, 'd'},
+            {"log",        no_argument,       0, 'e'},
+            {"display",    no_argument,       0, 'f'},
+            {0, 0, 0, 0}
+        };
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
 
-    // messenger = new MavlinkUDP("192.168.1.42", 14550);
-    // outputs.push_back(messenger);
-    // master->append_listener(messenger);
-    // messenger->append_listener(master);
-    //
-    messenger = new MavlinkUDP(14550);
-    outputs.push_back(messenger);
-    master->append_listener(messenger);
-    messenger->append_listener(master);
-    //
-    // messenger = new MavlinkSerial("/dev/ttyACM0", 57600);
-    // outputs.push_back(messenger);
-    // master->append_listener(messenger);
-    // messenger->append_listener(master);
-    //
-    // messenger = new MavlinkLogReader("log/log1");
-    // outputs.push_back(messenger);
-    // messenger->append_listener(master);
-    //
-    // messenger = new MavlinkLogWriter("log/");
-    // outputs.push_back(messenger);
-    // master->append_listener(messenger);
+        c = getopt_long (argc, argv, "abcdef", long_options, &option_index);
 
-    messenger = new MavlinkDisplay();
-    outputs.push_back(messenger);
-    master->append_listener(messenger);
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        MavMessengerInterface *messenger;
+        switch (c)
+        {
+            // case 0:
+            // {
+            //     /* If this option set a flag, do nothing else now. */
+            //     if (long_options[option_index].flag != 0)
+            //     break;
+            //     printf ("option %s", long_options[option_index].name);
+            //     if (optarg)
+            //     printf (" with arg %s", optarg);
+            //     printf ("\n");
+            //     break;
+            // }
+            case 'a':
+            {
+                messenger = new MavlinkSerial(optarg, 57600);
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            case 'b':
+            {
+                messenger = new MavlinkUDP(optarg, 14550);
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            case 'c':
+            {
+                messenger = new MavlinkUDP(atoi(optarg));//TODO test atoi
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            case 'd':
+            {
+                messenger = new MavlinkLogReader(optarg);
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            case 'e':
+            {
+                messenger = new MavlinkLogWriter("log/");
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            case 'f':
+            {
+                messenger = new MavlinkDisplay();
+                if(!master_assigned)
+                {
+                    master = messenger;
+                    master_assigned = true;
+                }
+                else
+                {
+                    outputs.push_back(messenger);
+                    master->append_listener(messenger);
+                    messenger->append_listener(master);
+                }
+                break;
+            }
+            // default:
+            //   abort ();
+        }
+    }
+
+      /* Instead of reporting ‘--verbose’
+         and ‘--brief’ as they are encountered,
+         we report the final status resulting from them. */
+    //   if (verbose_flag)
+    //     puts ("verbose flag is set");
+
+    /* Print any remaining command line arguments (not options). */
+    if (optind < argc)
+    {
+        printf ("non-option ARGV-elements: ");
+        while (optind < argc)
+        printf ("%s ", argv[optind++]);
+        putchar ('\n');
+    }
 
     master->join();
 
